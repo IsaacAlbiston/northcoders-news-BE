40f7f96b239a1489ffce9a1ff87e79c8eb54d45d
IsaacAlbiston <isaacalbiston@gmail.com>
2025-04-29 11:45:46 +0100

    implement PATCH /api/articles/article_id endpoint to increment the votes on a specified article

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express")
 const endpointsJson = require("./endpoints.json");
 const { handleInternalServerErr, handleCustomErr, handleSQLErr } = require("./app/controllers/error.controller"); 
 const { getTopics } = require("./app/controllers/topics.controller");
-const { getArticleById, getArticles } = require("./app/controllers/articles.controller");
+const { getArticleById, getArticles, patchArticle } = require("./app/controllers/articles.controller");
 const { getCommentsByArticleId, postCommentToArticleId } = require("./app/controllers/comments.controller");
 
 const app = express()
@@ -22,6 +22,8 @@ app.get("/api/articles/:article_id", getArticleById)
 app.get("/api/articles/:article_id/comments", getCommentsByArticleId)
 
 app.post("/api/articles/:article_id/comments", postCommentToArticleId)
+
+app.patch("/api/articles/:article_id", patchArticle)
 
 app.all('/*splat', (req,res)=>{
     res.status(404).send({msg:"Endpoint Not Found"})
