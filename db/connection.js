3c9bd51b6259fd1c701d46bff97153fa59c2d1e9
IsaacAlbiston <isaacalbiston@gmail.com>
2025-04-30 11:14:02 +0100

    set up hosting on supabase

--- db/connection.js
+++ db/connection.js
@@ -2,15 +2,22 @@ const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || 'development'
 
+const config = {}
+
+if (ENV === "production"){
+    config.connectionString = process.env.DATABASE_URL
+    config.max = 2
+}
+
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
-const db = new Pool();
+const db = new Pool(config);
 
-if (!process.env.PGDATABASE) {
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("No PGDATABASE configured")
 } else { 
     console.log(`Connected to ${process.env.PGDATABASE}`)
 }
 
 
-module.exports = db;
+module.exports = db
